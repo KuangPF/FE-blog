e2d6ba38a11a4be0ad508ca363ce7628e73fa022
kuangpengfei <1633397286@qq.com>
2017-12-27 23:59:41 +0800

    update index.json && vue.json

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const debug = process.env.NODE_ENV !== 'production';
-const INDEX_ITEM_URL_BUILD = 'https://kuangpf.github.io/FE-blog/dist/static/data/data.json';
+const INDEX_ITEM_URL_BUILD = 'https://kuangpf.github.io/FE-blog/dist/static/data/index.json';
 const VUE_ITEM_URL_BUILD = 'https://kuangpf.github.io/FE-blog/dist/static/data/vue.json';
-const INDEX_ITEM_URL_DEV = '../../static/data/data.json';
+const INDEX_ITEM_URL_DEV = '../../static/data/index.json';
 const VUE_ITEM_URL_DEV = '../../static/data/vue.json';
 
 const indexItemUrl = debug ? INDEX_ITEM_URL_DEV : INDEX_ITEM_URL_BUILD;
