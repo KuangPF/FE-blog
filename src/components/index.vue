468c037e65bd28c7709bbc0eb96037fa1b89e251
kuangpengfei <kuangpengfei@youloft.com>
2017-12-27 13:34:13 +0800

    index add menu && label

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,15 +1,25 @@
 <template>
 	<div class="index" v-title="'index'">
 		<div class="header">
-			<div class="menu"><img src="../../static/index/menu.svg"></div>
+			<div class="menu" @click="menuShow"><img src="../../static/index/menu.svg"></div>
 			<div class="title">Front-End Blog</div>
+		</div>
+		<div class="menu-content font2" :class="{'menu-content-show':isMenuShow}">
+			<div class="view-wrap">
+				<span class="view-title">关于我</span>
+				<div class="view-div">{{menuItemList.aboutMe}}</div>
+				<span class="view-title">标签</span>
+				<div class="view-div tagcloud view-div-label">
+					<a :href="item.src" v-for="item in menuItemList.label" :class="item.colorClass">{{item.desc}}</a>
+				</div>
+			</div>
 		</div>
 		<div class="main-content" ref="mainContent">
 			<div class="wrap">
 				<div class="content-item" v-for="item in indexItemList">
 					<div class="item-icon"><img :src="item.headerImg"></div>
 					<div class="item-detail">
-						<div class="item-label">
+						<div class="item-label tagcloud">
 							<span class="label-title">标签:</span>
 							<a v-for="item in item.label" :class="item.colorClass">{{item.labelText}}</a>
 						</div>
@@ -25,6 +35,7 @@
 				</div>
 			</div>
 		</div>
+		<div class="mask" v-if="isMaskShow" @click="hideMenu"></div>
 	</div>
 </template>
 
@@ -34,14 +45,19 @@ import BScroll from 'better-scroll';
 export default {
 	data() {
 		return {
-			'indexItemList': []
+			indexItemList: [],
+			menuItemList: [],
+			isMenuShow: false,
+			isMaskShow: false
 		};
 	},
 	mounted() {
 		getIndexItemLists().then(res => {
 			let indexItemList = res.data.indexItemList;
+			let menuItemList = res.data.menuItemList;
 			console.log(indexItemList);
 			this.indexItemList = indexItemList;
+			this.menuItemList = menuItemList;
 		});
 		this._initScroll();
 	},
@@ -50,6 +66,14 @@ export default {
 			this.mainContent = new BScroll(this.$refs.mainContent, {
 				click: true
 			});
+		},
+		menuShow() {
+			this.isMaskShow = true;
+			this.isMenuShow = true;
+		},
+		hideMenu() {
+			this.isMaskShow = false;
+			this.isMenuShow = false;
 		}
 	}
 };
